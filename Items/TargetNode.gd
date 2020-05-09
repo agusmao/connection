d7576155ef21d01310015b37eab5982ecfd5e125
Arthur Gusmão <arthur.t.a.gusmao@gmail.com>
2020-05-09 19:25:06 -0300

    Only add nodes when energized

--- Items/TargetNode.gd
+++ Items/TargetNode.gd
@@ -2,5 +2,3 @@ extends "res://Items/ConnectionNode.gd"
 
 func _on_ConnectionNode_body_entered(body):
 	get_tree().call_group("GameController", "addConnection", self, ConnectionTypes.GOAL)
-	# Level was completed
-	get_tree().call_group("GameController", "endGame")

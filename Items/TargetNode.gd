62ab44935356b850c4addad226c84ddac0b820da
Arthur Gusmão <arthur.t.a.gusmao@gmail.com>
2020-05-16 09:55:22 -0300

    Lamp audio

--- Items/TargetNode.gd
+++ Items/TargetNode.gd
@@ -4,4 +4,5 @@ func _on_ConnectionNode_body_entered(body):
 	get_tree().call_group("GameController", "addConnection", self, ConnectionTypes.GOAL)
 	
 func toggleConnection(toggle):
+	$AudioStreamPlayer2D.play()
 	$AnimatedSprite.play("on")

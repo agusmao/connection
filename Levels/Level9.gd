e8af6e355a59063083225bd6a49b598ff85bd967
Felipe Saruhashi <ffsaruhashi@gmail.com>
2020-05-16 22:09:33 +0200

    creates the level 9

---
+++ Levels/Level9.gd
@@ -0,0 +1,22 @@
+extends "res://Levels/LevelTemplate.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	var rotation = $YSort/Nodes.rotation_degrees
+	var diff
+	
+	for child in $YSort/Nodes.get_children():
+		child.global_rotation = 0
+	
+	for child in $YSort/Blockers.get_children():
+		child.global_rotation = 0
